b8a2211f6c831ea320846810393d681b9ee52428
devansh gupta <147030326+borderking20@users.noreply.github.com>
2024-10-20 22:19:26 +0530

    Add files via upload

---
+++ 900questions/A_AvtoBus.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define boost ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long int
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+#define pii pair<int,int>
+#define pll pair<long long int,long long int>
+#define pff pair<float,float>
+#define pdd pair<double,double>
+#define mod 1000000007
+#define PI 3.1415926535897932384626
+#define pb push_back
+#define mp make_pair
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+ll power(ll  b,ll p,ll modn=INT64_MAX)
+{
+if(p==0)
+return 1;
+ll temp=power(b,p/2,modn);
+temp=(temp*temp)%modn;
+if(p%2==1)
+temp=(temp*b)%modn;
+return temp;
+}
+string revstring(string s)
+{
+string t=s;
+reverse(all(t));
+return t;
+}
+void solve()
+{
+ ll n;
+    cin>>n;
+    if(n&1 || n<=3) cout<<-1<<endl;
+    else{
+        n=n/2;
+        ll temp1=n/3;
+        if(n%3==2 || n%3==1)temp1++;
+        ll temp2=n/2;
+        cout<<min({temp1,temp2})<<" "<<max({temp1,temp2})<<endl;
+    }
+}
+int main()
+{
+boost
+int t=1;
+cin>>t;
+while(t--)
+{
+solve();
+}
+return 0;
+}
